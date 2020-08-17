38c8bdbc35a3d95431697afba200999252ee1a11
Nishant Painter <n.painter@axelor.com>
2020-08-17 09:27:01 +0530

    Add euler solution 1

---
+++ project-euler/problem-1/solution-1.js
@@ -0,0 +1,9 @@
+const NumberUtils = require("../../utils/number-utils");
+const { getMultiplesOf, getSumOf, getUnique } = NumberUtils;
+
+const number = 999;
+const multiplesOf3 = getMultiplesOf(3, number);
+const multiplesOf5 = getMultiplesOf(5, number);
+const multiples = multiplesOf3.concat(multiplesOf5);
+const uniqueMultiples = getUnique(multiples);
+const sum = getSumOf(uniqueMultiples);
